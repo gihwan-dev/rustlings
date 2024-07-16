4c2aabf165d67ba69f59388cff1020b37a0ed816
CHOI GIHOAWN <rlghks3004@gmail.com>
2024-07-16 23:55:18 +0900

    day5

--- exercises/18_iterators/iterators5.rs
+++ exercises/18_iterators/iterators5.rs
@@ -23,38 +23,40 @@ enum Progress {
 }
 
 fn count_for(map: &HashMap<String, Progress>, value: Progress) -> usize {
-    let mut count = 0;
-    for val in map.values() {
-        if val == &value {
-            count += 1;
-        }
-    }
-    count
+    map.values()
+        .filter(|&val| val == &value)
+        .collect::<Vec<_>>()
+        .len()
 }
 
 fn count_iterator(map: &HashMap<String, Progress>, value: Progress) -> usize {
     // map is a hashmap with String keys and Progress values.
     // map = { "variables1": Complete, "from_str": None, ... }
-    todo!();
+    map.values()
+        .filter(|&val| val == &value)
+        .collect::<Vec<_>>()
+        .len()
 }
 
 fn count_collection_for(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
-    let mut count = 0;
-    for map in collection {
-        for val in map.values() {
-            if val == &value {
-                count += 1;
-            }
-        }
-    }
-    count
+    collection
+        .iter()
+        .flat_map(|elem| elem.values())
+        .filter(|&val| val == &value)
+        .collect::<Vec<_>>()
+        .len()
 }
 
 fn count_collection_iterator(collection: &[HashMap<String, Progress>], value: Progress) -> usize {
     // collection is a slice of hashmaps.
     // collection = [{ "variables1": Complete, "from_str": None, ... },
     //     { "variables2": Complete, ... }, ... ]
-    todo!();
+    collection
+        .iter()
+        .flat_map(|elem| elem.values())
+        .filter(|&val| val == &value)
+        .collect::<Vec<_>>()
+        .len()
 }
 
 #[cfg(test)]
